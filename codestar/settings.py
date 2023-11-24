91d81287a00c6387386faf359c6add2142dd242b
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-11-24 19:32:58 +0000

    Add view and template for index.html

--- codestar/settings.py
+++ codestar/settings.py
@@ -140,10 +140,10 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
- 
+
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
