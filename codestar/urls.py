2197e3ffd3277dfb19497f8aa9a165fb0bdcf385
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-11-25 08:24:12 +0000

    Add remove static from urls.py

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +22,3 @@ urlpatterns = [
     path('', include('blog.urls'), name='blog_url'),
     path('accounts/', include('allauth.urls')),
 ]
-
-urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
